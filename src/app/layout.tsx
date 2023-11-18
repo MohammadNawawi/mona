2947368edee4a4cd05395c8e741c62258f3af562
Mohammad Nawawi <tikp2.e24m.nawawi@gmail.com>
2023-11-18 19:06:48 +0700

    feat(UI) : Dark and Light Mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,8 @@ import { Inter } from "next/font/google";
 import { useState } from "react";
 import { usePathname } from "next/navigation";
 import Navbar from "@/components/layouts/navbar";
+import { AppProps } from "next/app";
+import { ThemeProvider } from "@/components/theme/theme-provider";
 // import { SessionProvider } from "next-auth/react";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -14,18 +16,24 @@ const disableNavbar = ["/login", "/register"];
 
 export default function RootLayout({
   children,
+  component,
+  pageProps,
 }: {
   children: React.ReactNode;
+  component: AppProps;
+  pageProps: AppProps;
 }) {
   const pathname = usePathname();
   return (
     <html lang="en">
       <body className={inter.className}>
-        {/* <SessionProvider> */}
-        {!disableNavbar.includes(pathname) && <Navbar />}
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          {/* <SessionProvider> */}
+          {!disableNavbar.includes(pathname) && <Navbar />}
 
-        {children}
-        {/* </SessionProvider> */}
+          {children}
+          {/* </SessionProvider> */}
+        </ThemeProvider>
       </body>
     </html>
   );
--- src/components/layouts/navbar.tsx
+++ src/components/layouts/navbar.tsx
@@ -1,16 +1,34 @@
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState, useEffect } from "react";
+import { useTheme } from "next-themes";
+import {
+  SunIcon,
+  MoonIcon,
+  MagnifyingGlassIcon,
+} from "@heroicons/react/24/outline";
 
 export default function Navbar() {
   const pathname = usePathname();
+
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
   return (
     <nav className="mx-auto flex max-w-6xl gap-8 px-6 transition-all duration-200 ease-in-out lg:px-12 py-4 justify-between">
       <div className="relative flex items-center ">
         <a href="/">
           <Image
-            alt=""
-            // src="https://www.svgrepo.com/show/499831/target.svg"
+            alt="Mona"
             src="/mona.svg"
             loading="lazy"
             width="20"
@@ -21,7 +39,11 @@ export default function Navbar() {
       <div className="flex">
         <ul className="hidden items-center justify-center gap-6 md:flex">
           <Link href="/code">
-            <li className="font-dm text-sm font-medium dark:text-white hover:text-violet-500">
+            <li
+              className={`font-dm text-sm font-medium dark:text-white hover:text-violet-500 ${
+                pathname === "/code" ? "text-violet-500" : ""
+              }`}
+            >
               Code
             </li>
           </Link>
@@ -39,51 +61,35 @@ export default function Navbar() {
         {/* <div className="flex-grow"></div> */}
         <div className="flex ml-2">
           <div className="hidden items-center justify-center gap-6 md:flex">
-            <a
-              href="#"
-              className="font-dm text-sm font-medium text-slate-700 ml-4"
-            >
+            <p className="font-dm text-sm font-medium text-slate-400 ml-4 select-none">
               |
-            </a>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={3}
-              stroke="currentColor"
-              className="w-4 h-4  hover:text-violet-500 hover:cursor-pointer"
+            </p>
+            <MagnifyingGlassIcon
+              width={16}
+              height={16}
+              className="hover:text-violet-500 cursor-pointer stroke-2"
+            />
+            <button
+              onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-              />
-            </svg>
-
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={3}
-              stroke="currentColor"
-              className="w-4 h-4 hover:text-violet-500 hover:cursor-pointer"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
-              />
-            </svg>
-
-            {/* <a
-              href="#"
-              className="rounded-md bg-gradient-to-br from-green-600 to-emerald-400 px-3 py-1.5 font-dm text-sm font-medium text-white shadow-md shadow-green-400/50 transition-transform duration-200 ease-in-out hover:scale-[1.03]"
-            >
-              Sign up for free
-            </a> */}
+              {theme === "dark" ? (
+                <MoonIcon
+                  width={16}
+                  height={16}
+                  className="hover:text-violet-700 cursor-pointer stroke-2 text-violet-500"
+                />
+              ) : (
+                <SunIcon
+                  width={20}
+                  height={20}
+                  className="hover:text-violet-700 cursor-pointer stroke-2 text-violet-500"
+                />
+              )}
+            </button>
           </div>
         </div>
       </div>
+
       <div className="relative flex items-center justify-center md:hidden gap-5">
         <button type="button">
           <svg
