e361d9f51382fb264d341a0e3c12a3c32089bc86
Mohammad Nawawi <tikp2.e24m.nawawi@gmail.com>
2023-11-21 16:24:59 +0700

    feat(UI) : Series Section

--- src/app/series/page.tsx
+++ src/app/series/page.tsx
@@ -1,7 +1,11 @@
 export default function SeriesPage() {
   return (
     <main className="mx-auto flex max-w-6xl gap-8 px-6 transition-all duration-200 ease-in-out lg:px-12 py-4">
-      <h1>Series Page</h1>
+      <section className="w-screen mt-10">
+        <div className="bg-red-300">
+          <h1 className="text-center text-xl font-bold">Series</h1>
+        </div>
+      </section>
     </main>
   );
 }
