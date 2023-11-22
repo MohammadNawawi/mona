ada38bc63813596a9cbb83bc02652d1e6dd4b5ac
Mohammad Nawawi <tikp2.e24m.nawawi@gmail.com>
2023-11-22 11:16:37 +0700

    feat(UI) : Series Page

--- src/app/series/page.tsx
+++ src/app/series/page.tsx
@@ -1,9 +1,34 @@
+import Image from "next/image";
+
 export default function SeriesPage() {
   return (
     <main className="mx-auto flex max-w-6xl gap-8 px-6 transition-all duration-200 ease-in-out lg:px-12 py-4">
-      <section className="w-screen mt-10">
-        <div className="bg-red-300">
-          <h1 className="text-center text-xl font-bold">Series</h1>
+      {/* <section className="w-screen mt-10 border-solid border-2 border-violet-500 rounded-lg"> */}
+      <section className="w-screen lg:mt-10 bg-white dark:bg-black dark:shadow-violet-500 drop-shadow-lg  rounded-lg">
+        <div className="p-10 flex justify-between">
+          <Image
+            src="/images/run.png"
+            alt="Series Run"
+            width={100}
+            height={100}
+            className="w-24 h-24 hidden lg:block"
+          />
+          <div className="flex flex-col text-center items-center justify-center">
+            <h1 className="text-xl font-bold ">Series</h1>
+            <p className="text-gray-400">
+              We are like books. Most people only see our cover, the minority
+              read only the introduction, <br />
+              many people believe the critics. Few will know our content.
+            </p>
+          </div>
+
+          <Image
+            src="/images/jumping-air.png"
+            alt="Series Jumping"
+            width={100}
+            height={100}
+            className="w-24 h-24 hidden lg:block"
+          />
         </div>
       </section>
     </main>
