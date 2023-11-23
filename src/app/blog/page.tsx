9a8cb23b3d904f44bacd7f65ac1f953984e84fc7
Mohammad Nawawi <tikp2.e24m.nawawi@gmail.com>
2023-11-23 19:50:35 +0700

    feat(UI) : Header Blog & Code

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,36 @@
+import Image from "next/image";
+
 export default function BlogPage() {
   return (
     <main className="mx-auto flex max-w-6xl gap-8 px-6 transition-all duration-200 ease-in-out lg:px-12 py-4">
-      <h1>Blog Page</h1>
+      {/* <section className="w-screen mt-10 border-solid border-2 border-violet-500 rounded-lg"> */}
+      <section className="w-screen lg:mt-10 bg-white dark:bg-black dark:shadow-violet-500 drop-shadow-lg  rounded-lg">
+        <div className="p-10 flex justify-between">
+          <Image
+            src="/images/bueno.png"
+            alt="Blog - Bueno"
+            width={100}
+            height={100}
+            className="w-24 h-24 hidden lg:block"
+          />
+          <div className="flex flex-col text-center items-center justify-center">
+            <h1 className="text-xl font-bold ">Blog</h1>
+            <p className="text-gray-400">
+              I write to give myself strength. I write to be a characters that i
+              am not.
+              <br /> I write to explore all the things i'm afraid of.
+            </p>
+          </div>
+
+          <Image
+            src="/images/whoa.png"
+            alt="Blog - Whoa"
+            width={100}
+            height={100}
+            className="w-24 h-24 hidden lg:block"
+          />
+        </div>
+      </section>
     </main>
   );
 }
--- src/app/code/page.tsx
+++ src/app/code/page.tsx
@@ -1,7 +1,35 @@
+import Image from "next/image";
+
 export default function CodePage() {
   return (
     <main className="mx-auto flex max-w-6xl gap-8 px-6 transition-all duration-200 ease-in-out lg:px-12 py-4">
-      <h1>Code Page</h1>
+      {/* <section className="w-screen mt-10 border-solid border-2 border-violet-500 rounded-lg"> */}
+      <section className="w-screen lg:mt-10 bg-white dark:bg-black dark:shadow-violet-500 drop-shadow-lg  rounded-lg">
+        <div className="p-10 flex justify-between">
+          <Image
+            src="/images/groceries.png"
+            alt="Code - Groceries"
+            width={100}
+            height={100}
+            className="w-24 h-24 hidden lg:block"
+          />
+          <div className="flex flex-col text-center items-center justify-center">
+            <h1 className="text-xl font-bold ">Code</h1>
+            <p className="text-gray-400">
+              Any fool can write code that a computer can understand.
+              <br /> Good programmers write code that humans can understand.
+            </p>
+          </div>
+
+          <Image
+            src="/images/gamestation.png"
+            alt="Code - Gamestation"
+            width={100}
+            height={100}
+            className="w-24 h-24 hidden lg:block"
+          />
+        </div>
+      </section>
     </main>
   );
 }
