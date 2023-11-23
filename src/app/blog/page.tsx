9e8eaaabb1f44a9093712ac31e178885e2fb6453
Mohammad Nawawi <tikp2.e24m.nawawi@gmail.com>
2023-11-23 20:08:21 +0700

    feat(UI) : Header Blog, Code & Series

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -3,7 +3,6 @@ import Image from "next/image";
 export default function BlogPage() {
   return (
     <main className="mx-auto flex max-w-6xl gap-8 px-6 transition-all duration-200 ease-in-out lg:px-12 py-4">
-      {/* <section className="w-screen mt-10 border-solid border-2 border-violet-500 rounded-lg"> */}
       <section className="w-screen lg:mt-10 bg-white dark:bg-black dark:shadow-violet-500 drop-shadow-lg  rounded-lg">
         <div className="p-10 flex justify-between">
           <Image
@@ -14,11 +13,11 @@ export default function BlogPage() {
             className="w-24 h-24 hidden lg:block"
           />
           <div className="flex flex-col text-center items-center justify-center">
-            <h1 className="text-xl font-bold ">Blog</h1>
+            <h1 className="text-xl font-bold">Blog</h1>
             <p className="text-gray-400">
               I write to give myself strength. I write to be a characters that i
               am not.
-              <br /> I write to explore all the things i'm afraid of.
+              <br /> I write to explore all the things i&apos;m afraid of.
             </p>
           </div>
 
--- src/app/series/page.tsx
+++ src/app/series/page.tsx
@@ -3,7 +3,6 @@ import Image from "next/image";
 export default function SeriesPage() {
   return (
     <main className="mx-auto flex max-w-6xl gap-8 px-6 transition-all duration-200 ease-in-out lg:px-12 py-4">
-      {/* <section className="w-screen mt-10 border-solid border-2 border-violet-500 rounded-lg"> */}
       <section className="w-screen lg:mt-10 bg-white dark:bg-black dark:shadow-violet-500 drop-shadow-lg  rounded-lg">
         <div className="p-10 flex justify-between">
           <Image
@@ -14,7 +13,7 @@ export default function SeriesPage() {
             className="w-24 h-24 hidden lg:block"
           />
           <div className="flex flex-col text-center items-center justify-center">
-            <h1 className="text-xl font-bold ">Series</h1>
+            <h1 className="text-xl font-bold">Series</h1>
             <p className="text-gray-400">
               We are like books. Most people only see our cover, the minority
               read only the introduction, <br />
