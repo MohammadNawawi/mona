01f05ccb508bff62cfa2e299ccb1eedabae29aca
Mohammad Nawawi <tikp2.e24m.nawawi@gmail.com>
2023-11-28 16:27:01 +0700

    feat(UI) : Footer Edit

--- src/components/layouts/footer.tsx
+++ src/components/layouts/footer.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Footer() {
   return (
-    <footer className="mx-auto max-w-6xl gap-8 px-6 transition-all duration-200 ease-in-out lg:px-12 py-4 justify-between bg-violet-300">
-      <div className="grid lg:grid-cols-3 gap-8 py-10">
+    <footer className="w-full bg-neutral-50 ">
+      <div className="mx-auto max-w-6xl gap-8 px-6 transition-all duration-200 ease-in-out lg:px-12justify-between grid lg:grid-cols-3 py-10">
         <div className="">
           <Image
             src="/mona.svg"
@@ -16,25 +16,13 @@ export default function Footer() {
             When you help someone, you help everyone.
           </p>
         </div>
-        <h1 className="select-none">Read Me</h1>
-        <h1 className="select-none">Contact Us</h1>
-      </div>
-      {/* <div className="flex justify-between">
-        <div className="basis w-3/12">
-          <Image
-            src="/mona.svg"
-            alt="logo"
-            width={100}
-            height={100}
-            className="w-5 h-5"
-          />
-          <p className="mt-1 text-sm text-gray-500">
-            When you help someone, you help everyone.
-          </p>
+        <div>
+          <h1 className="select-none">Read Me</h1>
         </div>
-        <h1>Ini footer</h1>
-        <h1>Ini footer</h1>
-      </div> */}
+        <div>
+          <h1 className="select-none">Contact Us</h1>
+        </div>
+      </div>
     </footer>
   );
 }
