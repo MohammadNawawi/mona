3507f582f6300c73d50c3872378bf0d9130750c6
Mohammad Nawawi <tikp2.e24m.nawawi@gmail.com>
2023-12-07 21:15:57 +0700

    feat(UI) : Register

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,5 +1,113 @@
 "use client";
 
+import Image from "next/image";
+import Link from "next/link";
+
 export default function RegisterPage() {
-  return <h1>Register Page</h1>;
+  return (
+    <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+      <Link
+        href="/"
+        className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white  "
+      >
+        <Image
+          width={100}
+          height={100}
+          className="w-8 h-8 mr-2"
+          src="/mona.svg"
+          alt="logo"
+        />
+      </Link>
+      <div className="w-full rounded-lg dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-transparent dark:border-transparent">
+        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+          <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white text-center select-none">
+            Sign Up {/* <span className="text-pink-500">&#10084;</span> */}
+          </h1>
+          <form className="space-y-4 md:space-y-6" action="#">
+            <div>
+              <label
+                htmlFor="fullname"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Your Fullname
+              </label>
+              <input
+                type="fullname"
+                name="fullname"
+                id="fullname"
+                className=" bg-gray-50 focus:ring-1 focus:outline-none focus:ring-purple-500 border focus:border-gray-300 sm:text-sm rounded-lg block w-full p-2.5  dark:placeholder-gray-400 dark:text-white"
+                placeholder="John Thor"
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="email"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Your email
+              </label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                className=" bg-gray-50 focus:ring-1 focus:outline-none focus:ring-purple-500 border focus:border-gray-300 sm:text-sm rounded-lg block w-full p-2.5  dark:placeholder-gray-400 dark:text-white"
+                placeholder="name@monass.com"
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="password"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="••••••••"
+                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                required
+              />
+            </div>
+            <div className="flex items-center justify-between">
+              <div className="flex items-start">
+                <div className="flex items-center h-5">
+                  <input
+                    id="remember"
+                    aria-describedby="remember"
+                    type="checkbox"
+                    className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                    required
+                  />
+                </div>
+                <div className="ml-3 text-sm">
+                  <label
+                    htmlFor="remember"
+                    className="text-gray-500 dark:text-gray-300"
+                  >
+                    Remember me
+                  </label>
+                </div>
+              </div>
+              <a
+                href="#"
+                className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
+              >
+                Forgot password?
+              </a>
+            </div>
+            <button
+              type="submit"
+              className="w-full bg-violet-500 text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 hover:bg-violet-700 dark:focus:ring-primary-800"
+            >
+              Sign Up
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
